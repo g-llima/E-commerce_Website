e17482fefab06793e9f0131b3b8f07e25f8cddf1
Gabriel Lima <gabrielrtxon@gmail.com>
2022-02-16 18:45:33 -0300

    Fixed large product name bug

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,21 +5,26 @@ function Card({
   productImage,
   productName,
   productPrice,
-  productDescription,
   btnName,
   clickBtn,
+  onCart,
 }) {
   return (
     <div className="card">
       <img className="card__img" src={productImage} />
       <div className="card__details">
-        <div className="card__details__product">
-          <h4 className="card__details__product__title">{productName}</h4>
-          <p className="card__details__product__price">$ {productPrice}</p>
-        </div>
-        <p className="card__details__description">{productDescription}</p>
-        <button className="card__details__cartBtn" onClick={clickBtn}>
-          {btnName} <i className="fal fa-cart-plus"></i>
+        <h4 className="card__details__product__title">{productName}</h4>
+        <p className="card__details__product__price">$ {productPrice}</p>
+        <button
+          className={`card__details__cartBtn ${onCart ? "card__onCard" : null}`}
+          onClick={clickBtn}
+        >
+          {btnName}
+          <i
+            className={`${
+              onCart ? "fal fa-cart-arrow-down" : "fal fa-cart-plus"
+            } card__details__cartBtn__icon`}
+          ></i>
         </button>
       </div>
     </div>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import "./styles/Cards.css";
 import { useCart } from "react-use-cart";
@@ -9,20 +9,23 @@ const products = [
     productImageLink:
       "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-gold-select-201810?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1633027804000",
     productName: "Apple Macbook",
-    price: 99,
-    productDesc:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: 9900,
   },
   {
     id: 2,
     productImageLink:
       "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-gold-select-201810?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1633027804000",
-    productName: "Apple Macbook2",
-    price: 19,
-    productDesc:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    productName: "Elegant designed coffee plant for desktop and mac",
+    price: 1900,
   },
 ];
+
+function convertProductName(str) {
+  if (str.length >= 41) {
+    return str.substring(0, 41) + "...";
+  }
+  return str;
+}
 
 function Cards() {
   const { addItem, removeItem, inCart } = useCart();
@@ -33,10 +36,11 @@ function Cards() {
         <Card
           key={key}
           productImage={item.productImageLink}
-          productName={item.productName}
+          productName={convertProductName(item.productName)}
           productPrice={item.price}
           productDescription={item.productDesc}
           btnName={inCart(item.id) ? "Remove from Cart" : "Add to Cart"}
+          onCart={inCart(item.id)}
           clickBtn={() => {
             inCart(item.id) ? removeItem(item.id) : addItem(item);
           }}
